61d38aee4ff6b7f83fa20f2f0ad4e4a6db395d33
Aditya Raj Chaudhary <adi@Adityas-MacBook-Air.local>
2025-09-18 11:16:42 +0530

    TodoApp

---
+++ src/main/java/com/todo/todo/controller/TodoController.java
@@ -0,0 +1,63 @@
+package com.todo.todo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.todo.todo.model.TodoModel;
+import com.todo.todo.service.TodoService;
+
+
+
+@RestController
+@RequestMapping("/todo")
+public class TodoController {
+
+    @Autowired
+    private TodoService todoService;
+
+    @PostMapping
+    public TodoModel createTodo(@RequestBody TodoModel todo){
+    return todoService.createTodo(todo);
+    }
+    @GetMapping("/getAll")
+    public List<TodoModel> getAllTodos() {
+        return todoService.getList();
+    }
+
+    @PutMapping
+    public TodoModel updateTodo(@RequestBody TodoModel todo){
+        return todoService.updateTodo(todo);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteTodo(@PathVariable Long id){
+        todoService.deleteTodo(id);
+        return "Todo Deleted Successfully";
+    }
+
+    @PostMapping("/bulk")
+    public List<TodoModel> createBulkTodos(@RequestBody List<TodoModel> todo){
+        return todoService.createBulkTodos(todo);
+    }
+    
+    @PutMapping("/bulk")
+    public List<TodoModel> updateBulkTodos(@RequestBody List<TodoModel> todo){
+        return todoService.updateBulkTodos(todo);
+    }
+
+    @DeleteMapping("/bulkDelete")
+    public String deleteBulkTodos(@RequestBody List<TodoModel> todo){
+        todoService.deleteBulkTodos(todo);
+        return "Todos Bulk Deleted Successfully";
+    }
+
+}
---
+++ src/main/java/com/todo/todo/service/TodoService.java
@@ -0,0 +1,52 @@
+package com.todo.todo.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.todo.todo.model.TodoModel;
+
+@Service
+public class TodoService {
+    
+    private List<TodoModel> todoList = new ArrayList<>();
+
+    public TodoModel createTodo(TodoModel todo) {
+        todoList.add(todo);
+        return todo;
+    }
+
+    
+    public List<TodoModel> getList() {
+        return todoList;
+    }
+
+    public TodoModel updateTodo(TodoModel todo) {
+        for (int i = 0; i < todoList.size(); i++) {
+            if (todoList.get(i).getId().equals(todo.getId())) {
+                todoList.set(i, todo);
+                return todo;
+            }
+        }
+        return null;
+    }
+
+    public void deleteTodo(Long id) {
+        todoList.removeIf(t -> t.getId().equals(id));
+    }
+
+    public List<TodoModel> createBulkTodos(List<TodoModel> todo){
+        todoList.addAll(todo);
+        return todoList;
+    }
+    public List<TodoModel> updateBulkTodos(List<TodoModel> todo){
+        for (TodoModel todoModel : todo) {
+            updateTodo(todoModel);
+        }
+        return todo;
+    }
+    public void deleteBulkTodos(List<String> ids){
+        todoList.removeIf(t -> ids.contains(t.getId()));
+    }
+}
